70190ffcf62965939aaf3c8816d23e012868eb0a
Mik <126609662+MikeMoskva@users.noreply.github.com>
2026-06-08 20:35:02 +0300

    New change in js, css and html codes

--- js1_3d.js
+++ js1_3d.js
@@ -1,1 +1,13 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const block = document.querySelector('.card');
+    
+    if (block) {
+        document.addEventListener('mousemove', function(e) {
+            let dx = e.pageX - window.innerWidth / 2;
+            let dy = e.pageY - window.innerHeight / 2;
+            let angleX = 15 * dx / (window.innerWidth / 2);
+            let angleY = 15 * dy / (window.innerHeight / 2);
+            block.style.transform = `perspective(1200px) rotateX(${-angleY}deg) rotateY(${angleX}deg)`;
+        });
+    }
+});
